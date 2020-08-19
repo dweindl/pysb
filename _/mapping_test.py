599b5815b238d988e952c3676608d42c599f522d
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-08-19 11:37:21 +0200

    Update example

--- _/mapping_test.py
+++ _/mapping_test.py
@@ -2,15 +2,20 @@ from pysb import *
 
 model = Model()
 
-A = Monomer('A', ['s1'], {'s1': ['u', 'l']})
-B = Monomer('B', ['s1'], {'s1': ['u', 'l']})
+A = Monomer('A', ['s1', 's2'], {'s1': ['u', 'l'], 's2': ['u', 'l']})
+B = Monomer('B', ['s1', 's2'], {'s1': ['u', 'l'], 's2': ['u', 'l']})
 k = Parameter('k', 0.1)
 # r1 = Rule('r1', A() @ {'s1': 'a'} >> B() @ {'s1': 'a'}, k)
-r1 = Rule('r1', A(s1=StateReference('a')) >> B(s1=StateReference('a')), k)
+e1 = Expression('e1', 0.5*k)
+r1 = Rule('r1', A(s1=StateReference('a'), s2=StateReference('b'))
+          >> B(s1=StateReference('a'), s2=StateReference('b')), e1)
+r2 = Rule('r2',  A(s1=StateReference('a'), s2=StateReference('b'))
+          >> B(s1=StateReference('b'), s2=StateReference('a')), e1)
+
 
 a0 = Parameter('a0', 1)
-Initial(A({'s1':'l'}), a0)
-Initial(A({'s1':'u'}), a0)
+Initial(A({'s1':'l', 's2': 'u'}), a0)
+#Initial(A({'s1':'u'}), a0)
 
 
 
